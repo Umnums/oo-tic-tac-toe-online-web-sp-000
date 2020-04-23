bd46bc01ae0ac16a000ebd19705b3e127c90e313
Joseph Kao <josephtkao@gmail.com>
2020-04-23 23:40:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,10 +17,6 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-
-
-
-
   def position_taken?(index)
     if @board[index].include?("X") || @board[index].include?("O")
       return true
